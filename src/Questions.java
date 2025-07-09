d7b6a42d0202cd87c79da8e3a23d3816d4424e11
Rachna Thakur <rachnathakur475@gmail.com>
2025-07-09 15:56:29 +0530

    Update Questions.java

--- src/Questions.java
+++ src/Questions.java
@@ -1,4 +1,35 @@
 package PACKAGE_NAME;
+import java.util.List;
 
 public class Questions {
+    private String questionText;
+    private List<String> options;
+    private int correctAnswerIndex;
+
+    public Questions(String questionText, List<String> options, int correctAnswerIndex) {
+        this.questionText = questionText;
+        this.options = options;
+        this.correctAnswerIndex = correctAnswerIndex;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public int getCorrectAnswerIndex() {
+        return correctAnswerIndex;
+    }
+    public void displayQuestion() {
+        System.out.println("\n" + questionText);
+        for (int i = 0; i < options.size(); i++) {
+            System.out.println((i + 1) + ". " + options.get(i));
+        }
+    }
+    public boolean isCorrect(int userAnswerIndex) {
+        return userAnswerIndex == correctAnswerIndex;
+    }
 }
